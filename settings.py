4a0900c823257d80ffe16a8fe8a904633d28eb73
정우석 <wsj@linkhubcorp.com>
2023-12-09 16:51:12 +0900

    상태확인 예제 수정

--- settings.py
+++ settings.py
@@ -4,12 +4,12 @@
 카카오써트 API Python SDK Example
 
 - Python SDK 연동환경 설정방법 안내 : https://developers.barocert.com/guide/kakao/identity/python/getting-started/tutorial
-- 업데이트 일자 : 2023-11-27
+- 업데이트 일자 : 2023-12-09
 - 연동 기술지원 연락처 : 1600-9854
 - 연동 기술지원 이메일 : code@linkhubcorp.com
 
 <테스트 연동개발 준비사항>
-1) 17, 20 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)를
+1) 17, 20, 23번 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)와 이용기관코드(ClientCode)를
     연동신청 후 메일로 발급받은 인증정보를 참조하여 변경합니다.
 """
 
